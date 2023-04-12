e333980c9da06304997b1621a5b21b40a0d060cf
belynick <nikkill338@gmail.com>
2023-04-12 20:34:50 +0300

    Добавьте файлы проекта.

---
+++ Practical 5/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Practical_5
+{
+    class Program
+    {
+
+        static Queue<int> sharedQueue = new Queue<int>();
+        static object lockObject = new object();
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Thread producerThread = new Thread(Producer);
+            Thread consumerThread = new Thread(Consumer);
+
+            producerThread.Start();
+            consumerThread.Start();
+
+            producerThread.Join();
+            consumerThread.Join();
+        }
+
+        static void Producer()
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < 15; i++)
+            {
+                int number = random.Next(50);
+                lock (lockObject)
+                {
+                    sharedQueue.Enqueue(number);
+                    Console.WriteLine($"Producer produced {number}");
+                }
+                Thread.Sleep(300);
+            }
+        }
+
+        static void Consumer()
+        {
+            while (true)
+            {
+                lock (lockObject)
+                {
+                    if (sharedQueue.Count > 0)
+                    {
+                        int number = sharedQueue.Dequeue();
+                        Console.WriteLine($"Consumer consumed {number}");
+                    }
+                }
+                Thread.Sleep(300);
+            }
+        }
+    }
+}
---
+++ Practical 5_2/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Threading;
+
+namespace Practical_5_2
+{
+    class Program
+    {
+        static Semaphore semaphore = new Semaphore(2, 2);
+        static object lockObject = new object();
+        static int currentGreenLight = 0;
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Thread[] threads = new Thread[4];
+
+            for (int i = 0; i < threads.Length; i++)
+            {
+                int index = i;
+                threads[i] = new Thread(() => TrafficLight(index));
+                threads[i].Start();
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                Thread thread = new Thread(Car);
+                thread.Start();
+            }
+        }
+
+        static void TrafficLight(int index)
+        {
+            while (true)
+            {
+                lock (lockObject)
+                {
+                    if (index == currentGreenLight)
+                    {
+                        Console.WriteLine($"Світлофор {index} зелений");
+                        currentGreenLight = (currentGreenLight + 1) % 4;
+                        Monitor.PulseAll(lockObject);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Світлофор {index} червоний");
+                        Monitor.Wait(lockObject);
+                    }
+                }
+
+                Thread.Sleep(3000);
+            }
+        }
+
+        static void Car()
+        {
+            Console.WriteLine("Автомобіль наближається до перехрестя");
+
+            semaphore.WaitOne();
+            Console.WriteLine("Автомобіль виїхав на перехрестя");
+
+            Thread.Sleep(2000);
+
+            Console.WriteLine("Ліве перехрестя автомобіля");
+            semaphore.Release();
+        }
+    }
+}
